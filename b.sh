af92323e7215733678a1188af35dc65442f55a6a
Skub <skub007@proton.me>
2024-06-06 17:19:46 +0530

    updated name to nokoist

--- b.sh
+++ b.sh
@@ -1,3 +1,3 @@
-g++ -std=c++17 -o todoist main.cpp
-x86_64-w64-mingw32-g++ -std=c++17 -o todoist.exe main.cpp
-./todoist
+g++ -std=c++17 -o nokoist main.cpp
+x86_64-w64-mingw32-g++ -std=c++17 -o nokoist.exe main.cpp
+./nokoist
